5c1997ab82a7f6a5ba2a1eaf25a296dd62cdccce
Dhruv Rohatgi <rastogidhruv28@gmail.com>
2024-07-06 17:14:57 +0530

    ..

--- NotesSharingWeb/NotesSharingWeb/settings.py
+++ NotesSharingWeb/NotesSharingWeb/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from pathlib import Path
-import dj_database_url
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
